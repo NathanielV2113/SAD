164cb283dd18e7c5c7bb131416a4e35bc13ffefe
Nathaniel <nathanielvasquez329@gmail.com>
2024-11-10 18:50:43 +0800

    latest

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function (){
+    Route::get('register', 'register')->name('register');
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.auth');
+    Route::post('register', 'registerSave')->name('register.auth');
 });
